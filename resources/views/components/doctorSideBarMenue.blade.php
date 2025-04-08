f39545a344ad002853a93b16885361934b07e5fb
Harish18200 <your-harishkutty300@gmail.com>
2025-04-08 08:57:46 +0530

    disease added

--- resources/views/components/doctorSideBarMenue.blade.php
+++ resources/views/components/doctorSideBarMenue.blade.php
@@ -25,9 +25,9 @@
             <li class="nav-item">
                 <a class="nav-link {{ Route::currentRouteName() === 'dashboard.import.nsapScheme.show' ? 'fw-bold text-white' : '' }}" href="{{route('viewAppointment')}}">View Appointment</a>
             </li>
-            <!-- <li class="nav-item">
+      `      <li class="nav-item">
                 <a class="nav-link {{ Route::currentRouteName() === 'dashboard.import.nsapScheme.show' ? 'fw-bold text-white' : '' }}" href="{{route('diseasesView')}}">Diseases</a>
-            </li> -->
+            </li>`
             <!-- <li class="nav-item">
                 <a class="nav-link {{ Route::currentRouteName() === 'dashboard.import.nsapScheme.show' ? 'fw-bold text-white' : '' }}" href="{{route('promptSearch')}}">Prompt</a>
             </li> -->
